9e78219dfd8e9a19713a728e6fddf18b295cc061
Harjot <harjotsingh2k19@gmail.com>
2022-08-09 21:29:33 +0530

    this is my first react app
    TextUtils functionality can be utilised from here

---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Harry";
+const b = "Rohan";
+const c = "Ajay";
+const d = "Aakash";
+
+export default b;
+export {a};
+export {c};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+// import About from "./components/About";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
+// let name = "Harry";
+// we cannot bold using this let name = "<b>harry</b>";
+// It will render as it is
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+
+  const showAlert = (message,type) =>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#05112dde';
+      showAlert("Dark Mode has been Enabled","success");
+      document.title = 'TextUtils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been Enabled","success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* props */}
+
+      {/* <Router> */}
+      <Navbar title="TextUtils" about=" About Us" mode={mode} toggleMode = {toggleMode}/>
+      <Alert alert = {alert}/>
+      {/* <Navbar /> */}
+
+      <div className="container my-3">
+      {/* <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route> */}
+          {/* <Route exact path="/"> */}
+            <TextForm showAlert = {showAlert} heading="Enter the text to analyse below" mode={mode}/>
+          {/* </Route>
+        </Switch> */}
+      </div>
+    {/* </Router> */}
+        
+    </>
   );
 }
 
